1387a274fc224bd198d04e2e5c62fd7f8bd94279
cyberbyte <66154594+thecyberbyte-tech@users.noreply.github.com>
2021-04-21 13:15:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
-API_ID = int(os.getenv("1619830"))
-API_HASH = os.getenv("cec48932ac81b0c898dff788d118ce19")
-BOT_TOKEN = os.getenv("1745769392:AAHBDZVEuadD9mluxa0PXiAoNkrf-s9Bi8E")
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 DATABASE_URL = os.getenv("DATABASE_URL")
-OWNER_ID = list({int(x) for x in os.environ.get("1087829737", "").split()})
+OWNER_ID = list({int(x) for x in os.environ.get("OWNER_ID", "").split()})
